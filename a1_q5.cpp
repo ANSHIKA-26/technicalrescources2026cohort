85e8505d12c1e35d08ccef66759af036b93c4777
ANSHIKA-26 <114569262+ANSHIKA-26@users.noreply.github.com>
2023-01-15 17:22:55 +0530

    submitting q5 of assignment 1

---
+++ a1_q5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int count_digit(int n) {
+   int count = 0;
+   while(n != 0) {
+      n = n/ 10;
+      count++;
+   }
+   return count;
+}
+int main() {
+    int n;
+    cin>>n;
+   cout <<count_digit(n)<< endl;
+}
